8c2f6fb8cadbc272bd030cc52be58f081442c674
matthewvandyek <leonidasmith@protonmail.com>
2021-10-22 06:42:02 +0200

    completely new vibes

--- js/work-desktop.js
+++ js/work-desktop.js
@@ -1,13 +1,11 @@
 const gallery = document.getElementById('gallery');
-// const image = document.createElement('img');
-// image.src = 'img/art/1.jpg';
-// gallery.appendChild(image);
-
 const popup = document.getElementById('popup');
 const selectedImage = document.getElementById('selectedImage');
 
-const imageIndexes = [1,2,3,4,5,6,7,8,9];
-// const selectedImage = null;
+const imageIndexes = [1,2,3,4,5,6,7,8,9,10];
+const selectedIndex = null;
+
+
 
 imageIndexes.forEach((i) => {
   const image = document.createElement('img');
@@ -25,7 +23,7 @@ imageIndexes.forEach((i) => {
 });
 
 popup.addEventListener('click', () => {
-  popup.style.transform = `translateY(-100%)`;
+  popup.style.transform = `translateY(-200%)`;
   popup.src = '';
   popup.alt = '';
 })
