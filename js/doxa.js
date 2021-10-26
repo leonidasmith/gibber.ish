74fafd466b5be1c728ff51e6909a69accd3b5b49
matthewvandyek <leonidasmith@protonmail.com>
2021-10-26 22:16:51 +0200

    doxa sketch

---
+++ js/doxa.js
@@ -0,0 +1,15 @@
+const article = document.getElementById('article');
+const selectedArticle = document.getElementById('selectedArticle');
+
+// Number of articles hidden in HTML code for leisure
+const N = document.getElementById('counter').textContent;
+const articleIndexes = Array.from({length: N}, (_, i) => i + 1);
+const selectedIndex = null;
+
+
+
+
+//
+// articleIndexes.forEach((i) => {
+//
+// });
